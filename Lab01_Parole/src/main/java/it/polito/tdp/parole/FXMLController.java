d76918de95c631d137001a8093108a1e3890d5ae
admin <admin@LAPTOP-7TJJINKV>
2020-03-24 12:33:19 +0100

    Svolgimento esercitazione 1

--- Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
@@ -1,7 +1,6 @@
 package it.polito.tdp.parole;
-
+ 
 import it.polito.tdp.parole.model.Parole;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -11,9 +10,8 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class FXMLController {
-	
-	Parole elenco ;
-
+    Parole elenco ;
+   
     @FXML
     private ResourceBundle resources;
 
@@ -27,28 +25,76 @@ public class FXMLController {
     private Button btnInserisci;
 
     @FXML
+    private Button btnCancella;
+
+    @FXML
     private TextArea txtResult;
 
     @FXML
     private Button btnReset;
+    
+    @FXML
+    private TextField txtTempoRisposta;
+
+    @FXML
+    void doDelete(ActionEvent event) {
+    	if(txtParola.getText().equals("") || txtParola.getText().contains("0") || txtParola.getText().contains("1") ||txtParola.getText().contains("2") ||txtParola.getText().contains("3") ||txtParola.getText().contains("4") ||txtParola.getText().contains("5") ||txtParola.getText().contains("6") ||txtParola.getText().contains("7") ||txtParola.getText().contains("8") ||txtParola.getText().contains("9"))
+    	   {
+    		   txtResult.setText("La parola non è nel dataset e non può essere eliminata!!!\n");
+    		   txtParola.setText("");
+    		   txtTempoRisposta.setText(""+System.nanoTime());
+    		   return;
+    	   }
+    	else if(elenco.contieneParola(txtParola.getText())==true)
+    		{
+    		elenco.eliminaParola(txtParola.getText());
+    		txtResult.setText(""+elenco.toString());
+    		txtTempoRisposta.setText(""+System.nanoTime());
+    		}
+    	else
+    	{
+    	   txtResult.setText("La parola non è nel dataset e non può essere eliminata!!!\n");
+ 		   txtParola.setText("");
+ 		   elenco.getElenco();
+ 		   txtTempoRisposta.setText(""+System.nanoTime());
+ 		   return;
+    	}
+    }
 
     @FXML
     void doInsert(ActionEvent event) {
-    	// TODO
+     if(txtParola.getText().equals(""))
+	   return;
+   if(txtParola.getText().contains("0") || txtParola.getText().contains("1") ||txtParola.getText().contains("2") ||txtParola.getText().contains("3") ||txtParola.getText().contains("4") ||txtParola.getText().contains("5") ||txtParola.getText().contains("6") ||txtParola.getText().contains("7") ||txtParola.getText().contains("8") ||txtParola.getText().contains("9"))
+   {
+	   txtResult.setText("Inserisci solo lettere cretino!!!\n");
+	   txtParola.setText("");
+	   return;
+   }
+   elenco.addParola(txtParola.getText());
+   txtParola.clear();
+   txtResult.setText(""+elenco.toString());
+   txtTempoRisposta.setText(""+System.nanoTime());
+
     }
 
     @FXML
     void doReset(ActionEvent event) {
-    	// TODO
+     	elenco.reset();
+    	txtResult.setText("");
+    	txtTempoRisposta.setText(""+System.nanoTime());
     }
 
     @FXML
     void initialize() {
         assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Scene.fxml'.";
         assert btnInserisci != null : "fx:id=\"btnInserisci\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCancella != null : "fx:id=\"btnCancella\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
         assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
-
+        assert txtTempoRisposta != null : "fx:id=\"txtTempoRisposta\" was not injected: check your FXML file 'Scene.fxml'.";
         elenco = new Parole() ;
     }
 }
+
+
--- Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
@@ -1,24 +1,51 @@
 package it.polito.tdp.parole.model;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Parole {
-		
+	
+	TreeMap <String,String> IP;
 	public Parole() {
 		//TODO
+		IP=new TreeMap <String,String> ();
 	}
-	
+
 	public void addParola(String p) {
-		//TODO
+    IP.put(p,p);
 	}
 	
 	public List<String> getElenco() {
-		//TODO
-		return null;
+		//TODO	
+		List <String> a= new ArrayList <String> (IP.values());
+		return a;
 	}
 	
 	public void reset() {
 		// TODO
+		IP.clear();
 	}
-
+	
+	public boolean contieneParola(String s) {
+		if(IP.containsKey(s))
+	    return true;
+		return false;
+	}
+	
+	public void eliminaParola(String s) {
+		IP.remove(s);
+	}
+	//METODO PER PASSARE DA I VALUES DI UNA MAPPA A UNA LINKED LIST
+	//METODO PER PASSARE DA UNA LINKED LIST ALLA SUA DESCRIZIOINE A STRINGA;
+	
+	public String toString() {
+		String cercata="";
+    List <String> a= new ArrayList <String> (IP.values());  
+    LinkedList <String> b=new LinkedList <String>();
+    for(int i=0;i<a.size();i++)
+    	b.add(a.get(i));
+    for (String s1:b)
+    	cercata+=s1+"\n";
+	return cercata;
+	}
 }
